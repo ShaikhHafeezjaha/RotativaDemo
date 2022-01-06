80a861a68f632a10e75f7c22ea325440c9ab710f
Hafeezjaha Shaikh <hafeeza.vision@gmail.com>
2022-01-06 14:31:36 +0530

    code

---
+++ Controllers/ExportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+using RotativeDemo.Models;
+
+namespace RotativeDemo.Controllers
+{
+    public class ExportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ViewAsPdf(Invoice.GetOne());
+        }
+    }
+}
